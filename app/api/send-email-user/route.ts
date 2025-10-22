9fe93055c8d0a1bbe3bd3ef7c20054df21a068a8
Tasnimul Hasan Mahi <mahitasnimul2@gmail.com>
2025-10-23 00:03:47 +0600

    update contact

--- app/api/send-email-user/route.ts
+++ app/api/send-email-user/route.ts
@@ -4,38 +4,36 @@ import ejs from "ejs";
 import path from "path";
 import { google } from "googleapis";
 
-
 export async function POST(req: Request) {
   try {
     const { name, email, subject, message } = await req.json();
 
     const oAuth2Client = new google.auth.OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-  );
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      "https://developers.google.com/oauthplayground"
+    );
 
-  oAuth2Client.setCredentials({
-    refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-  });
+    oAuth2Client.setCredentials({
+      refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+    });
 
-  const accessToken = await oAuth2Client.getAccessToken();
+    const accessToken = await oAuth2Client.getAccessToken();
 
-
-  const transporter = nodemailer.createTransport({
-    // host: "smtp.ionos.co.uk",
-    // port: 587,
-    // secure: false,
-    service: "Gmail",
-    auth: {
-      type: "OAuth2",
-      user: process.env.SENDER_EMAIL,
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-      accessToken: accessToken?.token || "",
-    },
-  });
+    const transporter = nodemailer.createTransport({
+      // host: "smtp.ionos.co.uk",
+      // port: 587,
+      // secure: false,
+      service: "Gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.SENDER_EMAIL,
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+        accessToken: accessToken?.token || "",
+      },
+    });
 
     // Render ejs template
     const templatePath = path.join(
@@ -43,14 +41,14 @@ export async function POST(req: Request) {
       "static/email_template/contact_user_template.ejs"
     );
     const html = await ejs.renderFile(templatePath, {
-     name,
-     email,
-     message,
-     subject
+      name,
+      email,
+      message,
+      subject,
     });
 
     const mailOptions = {
-      from: process.env.SENDER_EMAIL,
+      from: `Watney College <${process.env.SENDER_EMAIL}>`,
       // to: "mahitasnimul2@gmail.com",
       to: email,
       subject: `Thank You for Contacting Watney College`,
